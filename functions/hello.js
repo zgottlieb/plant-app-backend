30b3f4bd593a17f5117afd751a53064370319db9
Zach Gottlieb <zachg27@gmail.com>
2021-04-03 11:49:58 -0400

    add console.log

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,6 @@
 exports.handler = async (event) => {
-  const subject = event.queryStringParamters.name || "World";
+  console.log(event, event.queryStringParameters);
+  const subject = event.queryStringParameters.name || "World";
   return {
     statusCode: 200,
     body: `Hello ${subject}!`,
